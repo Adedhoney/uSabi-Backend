594fcf6e9ebe7b30b3866b323dd2ac2e3197617d
DYung26 <theadstore1407@gmail.com>
2024-12-08 07:27:20 +0100

    Refactor `FlashcardService`: Updated `createFlashcard` to return the created `Flashcard` object and improved method return types.

--- src/Service/FlashcardService.ts
+++ src/Service/FlashcardService.ts
@@ -1,3 +1,4 @@
+import { generateRandomId } from "@application/utilities";
 import { Flashcard } from "@domain/Models/Flashcards";
 import { FlashcardRepository } from "@domain/Repositories/FlashcardRepository";
 import { generateFlashcards } from "AI/services/aiFlashcardService";
@@ -5,7 +6,7 @@ import { generateFlashcards } from "AI/services/aiFlashcardService";
 export interface IFlashcardService {
     getFlashcards(): Promise<Flashcard[]>;
     getFlashcard(flashcardId: string): Promise<Flashcard>;
-    createFlashcard(flashcard: Flashcard): Promise<void>;
+    createFlashcard(flashcard: Flashcard): Promise<Flashcard>;
     updateFlashcard(flashcardId: string, flashcard: Partial<Flashcard>): Promise<void>;
 }
 
@@ -14,21 +15,24 @@ export class FlashcardService implements IFlashcardService {
 
     async getFlashcards(): Promise<Flashcard[]> {
         const flashcards = await this.flashcardrepo.getAllFlashcards();
-	return flashcards as Flashcard[];
+	return flashcards;
     }
 
     async getFlashcard(flashcardId: string): Promise<Flashcard> {
         const flashcard = await this.flashcardrepo.getFlashcardById(flashcardId);
-	return flashcard as Flashcard;
+	return flashcard;
     }
 
-    async createFlashcard(flashcard: Flashcard): Promise<void> {
+    async createFlashcard(flashcard: Flashcard): Promise<Flashcard> {
+	const flashcardId = generateRandomId();
+	flashcard.flashcardId = flashcardId;
+
 	const qas = await generateFlashcards(
             flashcard.category, flashcard.title, flashcard.avatar,
 	    flashcard.difficulty, flashcard.numberOfQuestions
 	);
 	flashcard.qas = qas;
-        await this.flashcardrepo.createFlashcard(flashcard);
+        return await this.flashcardrepo.createFlashcard(flashcard);
     }
 
     async updateFlashcard(flashcardId: string, flashcard: Partial<Flashcard>): Promise<void> {
