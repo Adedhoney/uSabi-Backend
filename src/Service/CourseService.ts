700fc3c508a5c69d7a48648bcd5fe08aa9d6d100
DYung26 <theadstore1407@gmail.com>
2024-12-09 10:49:34 +0100

    Implement a robust service layer for managing `courses` with `CRUD` operations and precise error handling for missing or empty datasets.

---
+++ src/Service/CourseService.ts
@@ -0,0 +1,50 @@
+import { CustomError } from "@application/error";
+import { StatusCode } from "@application/utilities";
+import { Course } from "@domain/Models/Course";
+import { ICourseRepository } from "@domain/Repositories/CourseRepository";
+
+export interface ICourseService {
+    createCourse(course: Course): Promise<Course>;
+    updateCourse(course: Partial<Course>): Promise<void>;
+    fetchAllCourses(): Promise<Course[]>;
+    fetchCourse(courseId:  string): Promise<Course>;
+    deleteCourse(courseId: string): Promise<void>;
+}
+
+export class CourseService implements ICourseService {
+    constructor(
+        private courseRepo: ICourseRepository,
+    ) {}
+
+    async createCourse(course: Course): Promise<Course> {
+        return await this.courseRepo.createCourse(course);
+    }
+
+    async updateCourse(course: Partial<Course>): Promise<void> {
+        await this.courseRepo.updateCourse(course);
+    }
+
+    async fetchAllCourses(): Promise<Course[]> {
+        const courses = await this.courseRepo.getAllCourses();
+	if (!courses.length) {
+	    throw new CustomError(
+		'No courses found', StatusCode.NOT_FOUND
+	    );
+	}
+	return courses as Course[];
+    }
+
+    async fetchCourse(courseId:  string): Promise<Course> {
+        const course = await this.courseRepo.getCourseById(courseId);
+	if (!course) {
+            throw new CustomError(
+		'Course not found', StatusCode.NOT_FOUND
+	    );
+	}
+	return course as Course;
+    }
+
+    async deleteCourse(courseId: string): Promise<void> {
+        await this.courseRepo.deleteCourseById(courseId);
+    }
+}
