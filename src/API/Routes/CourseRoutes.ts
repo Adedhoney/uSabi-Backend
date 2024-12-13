469614946fef3c5beec2f058bccfd5e8664385a0
DYung26 <theadstore1407@gmail.com>
2024-12-13 13:03:15 +0100

    `fix(CourseRoutes)`: Add route for fetching `chapters` in a `course` and include `ChapterController`

--- src/API/Routes/CourseRoutes.ts
+++ src/API/Routes/CourseRoutes.ts
@@ -1,14 +1,19 @@
+import { ChapterController } from "@api/Controller/ChapterController";
 import { CourseController } from "@api/Controller/CourseController";
 import { RequestHandler, Router } from "express";
 
 const router = Router();
 
-export default (courseController: CourseController, authentication: RequestHandler) => {
+export default (
+    courseController: CourseController, chapterController: ChapterController,
+    authentication: RequestHandler
+) => {
     router.post('/', authentication, courseController.createCourse);
     router.get('/', authentication, courseController.getAllCourses);
     router.get('/:courseId', authentication, courseController.getCourse);
     router.put('/:courseId', authentication, courseController.updateCourse);
     router.delete('/:courseId', authentication, courseController.deleteCourse);
+    router.get('/:courseId/chapters', authentication, chapterController.fetchChapter);
 
     return router;
 };
