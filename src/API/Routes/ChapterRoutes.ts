315501307efea5800d746c424949d759cacbf86e
DYung26 <theadstore1407@gmail.com>
2024-12-14 14:42:20 +0100

    Integrate video retrieval endpoint into `ChapterRoutes` for fetching videos within a specific chapter.

--- src/API/Routes/ChapterRoutes.ts
+++ src/API/Routes/ChapterRoutes.ts
@@ -1,13 +1,18 @@
 import { ChapterController } from "@api/Controller/ChapterController";
+import { VideoController } from "@api/Controller/VideoController";
 import { RequestHandler, Router } from "express";
 
 
 const router = Router();
 
-export default (chapterController: ChapterController, authentication: RequestHandler) => {
+export default (
+    chapterController: ChapterController, videoController: VideoController,
+    authentication: RequestHandler
+) => {
     router.post('/', authentication, chapterController.createChapter);
     router.get('/:chapterId/', authentication, chapterController.fetchChapter);
     router.put('/:chapterId/', chapterController.updateChapter);
+    router.get('/:chapterId/video', videoController.getVideo);
 
     return router;
 }
