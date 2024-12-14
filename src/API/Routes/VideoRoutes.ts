e47bb0549f4b7bf67309fe2a1548db5b14faaa9d
DYung26 <theadstore1407@gmail.com>
2024-12-14 14:48:07 +0100

    Add validation middleware for video upload in `VideoRoutes` to enforce schema compliance.

--- src/API/Routes/VideoRoutes.ts
+++ src/API/Routes/VideoRoutes.ts
@@ -1,10 +1,12 @@
 import { VideoController } from "@api/Controller/VideoController";
+import { Validation } from "@api/Middleware";
+import { VideoSchema } from "@api/Schemas";
 import { RequestHandler, Router } from "express";
 
 const router = Router();
 
 export default (videoController: VideoController, authentication: RequestHandler) => {
-    router.post('/', authentication, videoController.uploadVideo);
+    router.post('/', authentication, Validation(VideoSchema), videoController.uploadVideo);
     router.put('/:videoId', authentication, videoController.updateUploadedVideo);
     router.delete('/:videoId', authentication, videoController.deleteVideo);
 
