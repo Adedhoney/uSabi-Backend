c3eda2a553faf7de57ff34ef2c923907f8cb2121
DYung26 <theadstore1407@gmail.com>
2024-12-16 17:14:53 +0100

    Define `WaitlistUser` interface to represent users in the waitlist system

---
+++ src/Domain/Models/Waitlist.ts
@@ -0,0 +1,9 @@
+export interface WaitlistUser {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    emailVerified: boolean;
+    createdAt: number;
+    updatedAt?: number;
+}
