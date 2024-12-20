b550548a1b5854ff54e40589ef8416d083852501
DYung26 <theadstore1407@gmail.com>
2024-12-20 20:56:49 +0100

    Add `'permission'` field to `User` model and define `Permission` enum for role-based access control

--- src/Domain/Models/User.ts
+++ src/Domain/Models/User.ts
@@ -4,6 +4,7 @@ export interface User {
     email: string;
     firstName: string;
     lastName: string;
+    permission: Permission,
     status: UserAccountStatus;
     password?: string;
     emailVerified: boolean;
@@ -15,7 +16,12 @@ export interface User {
     learningStyle?: UserLearningStyle;
     aiResponse?: ResponseOption;
     learningDuration?: LearningSchedule;
-    avatar?: AIAvatarMentor;
+    avatar: AIAvatarMentor;
+}
+
+export enum Permission {
+    ADMIN = "admin",
+    USER = "user"
 }
 
 export enum UserAccountStatus {
