c61cf1baf47bb28ed94a35bae03719893d47aaed
DYung26 <theadstore1407@gmail.com>
2025-01-08 05:14:24 +0100

    Fix typos and `SQL` syntax issues in `UserCourseRepository`, ensure proper query destructuring, and correct parameter order in `updateUserCourse` method

--- src/Domain/Repositories/UserCourseRepository.ts
+++ src/Domain/Repositories/UserCourseRepository.ts
@@ -2,26 +2,28 @@ import { UserCourse } from "@domain/Models/UserCourse";
 import { IDatabase } from "@infrastructure/Database";
 
 
-export interface IUserCourseReporitory {
+export interface IUserCourseRepository {
     readonly db: IDatabase;
     enrol(userCourse: UserCourse): Promise<void>;
+    getEnrolledCourse(userId: string, courseId: string): Promise<UserCourse>;
     enrolledCourses(userId: string): Promise<UserCourse[]>;
     isUserEnrolled(userId: string, courseId: string): Promise<boolean>;
+    updateUserCourse(userCourse: Partial<UserCourse>): Promise<void>;
 }
 
-export class UserCourseRepository implements IUserCourseReporitory {
+export class UserCourseRepository implements IUserCourseRepository {
     constructor(readonly db: IDatabase) {}
     
     async enrol(userCourse: UserCourse): Promise<void> {
         await this.db.execute(
-	    `INSERT INTO user_courses (userId, courseId, createdAt, updatedAt) VALUES (?,?,?,?)`,
-	    [userCourse.userId, userCourse.courseId,
+	    `INSERT INTO user_courses (userId, courseId, completed, createdAt, updatedAt) VALUES (?,?,?,?,?)`,
+	    [userCourse.userId, userCourse.courseId, userCourse.completed,
 	    userCourse.enrolledAt, userCourse.updatedAt],
 	);
     }
 
     async getEnrolledCourse(userId: string, courseId: string): Promise<UserCourse> {
-        const enrolledCourse = await this.db.execute(
+        const [enrolledCourse] = await this.db.execute(
             `SELECT uc.*
 	    FROM user_courses uc
 	    WHERE userId=? AND courseId=?`,
@@ -42,13 +44,13 @@ export class UserCourseRepository implements IUserCourseReporitory {
     }
 
     async isUserEnrolled(userId: string, courseId: string): Promise<boolean> {
-        const result = await this.db.execute(
+        const [result] = await this.db.execute(
 	    `SELCT COUNT(*) as count
-	    FROM UserCourses
+	    FROM user_courses
 	    WHERE userId=? AND courseId=?`,
 	    [userId, courseId]
 	);
-	return result[0].count > 0;
+	return result.count > 0;
     }
 
     async updateUserCourse(userCourse: Partial<UserCourse>): Promise<void> {
@@ -61,7 +63,7 @@ export class UserCourseRepository implements IUserCourseReporitory {
             `UPDATE user_courses
 	    SET ${setClause}
 	    WHERE courseId=? AND userId=?`,
-	    [...values, userCourse.userId, userCourse.courseId]
+	    [...values, userCourse.courseId, userCourse.userId]
 	);
     }
 }
