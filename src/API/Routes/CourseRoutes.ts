712d394129ceeddada1286d5bee507b54edf50ec
DYung26 <theadstore1407@gmail.com>
2025-01-08 16:39:37 +0100

    Refactor `CourseRoutes` to include `UserCourseController` and admin authentication for course management

--- src/API/Routes/CourseRoutes.ts
+++ src/API/Routes/CourseRoutes.ts
@@ -1,21 +1,27 @@
 import { ChapterController } from "@api/Controller/ChapterController";
 import { CourseController } from "@api/Controller/CourseController";
+import { UserController } from "@api/Controller/UserController";
+import { UserCourseController } from "@api/Controller/UserCourseController";
 import { RequestHandler, Router } from "express";
 
 const router = Router();
 
 export default (
     courseController: CourseController, chapterController: ChapterController,
-    authentication: RequestHandler
+    userCourseController: UserCourseController, authentication: RequestHandler,
+    adminAuthentication?: RequestHandler
 ) => {
-    router.post('/', authentication, courseController.createCourse);
-    router.get('/', authentication, courseController.getAllCourses);
-    router.get('/:courseId', authentication, courseController.getCourse);
-    router.put('/:courseId', authentication, courseController.updateCourse);
-    router.delete('/:courseId', authentication, courseController.deleteCourse);
-    router.get('/:courseId/chapters', authentication, chapterController.fetchChapter);
+    if (adminAuthentication) {
+	router.post('/', authentication, adminAuthentication, courseController.createCourse);
+        router.get('/:courseId', authentication, adminAuthentication, courseController.getCourse);
+        router.put('/:courseId', authentication, adminAuthentication, courseController.updateCourse);
+        router.delete('/:courseId', authentication, adminAuthentication, courseController.deleteCourse);
+        router.get('/:courseId/chapters', authentication, adminAuthentication, chapterController.fetchChapter);
+    }
 
+    router.get('/', authentication, courseController.getAllCourses);
     router.get('/:courseId/full-details', authentication, courseController.getCourseDetails);
+    // get all courses full-details
 
     return router;
 };
