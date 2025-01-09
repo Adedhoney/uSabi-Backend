4a7cebdffdde5d1f83bffea4ca2041d73c4ace74
DYung26 <theadstore1407@gmail.com>
2025-01-09 03:30:51 +0100

    Add `avatar` field of type `AIAvatarMentor` to `Course` model

--- src/Domain/Models/Course.ts
+++ src/Domain/Models/Course.ts
@@ -1,8 +1,11 @@
+import { AIAvatarMentor } from "./User";
+
 export interface Course {
     courseId: string;
     category: string;
     topic: string;
     description?: string;
+    avatar: AIAvatarMentor;
     createdAt: number;
     updatedAt?: number;
 }
